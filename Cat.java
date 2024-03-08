3c5a79f5934122137b3ce522c9fbda9a63fbc9db
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-08 16:48:07 +0300

    Added isEnoughFood method in class Cat

--- Cat.java
+++ Cat.java
@@ -2,11 +2,16 @@ public class Cat {
     private String name;
     private int appetite;
     private boolean satiety; // сытость
-    
+
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
     }
+
+    public boolean isEnoughFood(Plate plate) { // достаточно ли еды
+        return plate.getFood() >= this.appetite;
+    }
+
     public void eat() { }
 
 }
