7ea9677bb8e29df6ac983a2e6b4ee00af59fa3b5
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-08 16:50:58 +0300

    Modified class BaseCat

--- BaseCat.java
+++ BaseCat.java
@@ -1,3 +1,18 @@
 public class BaseCat {
 
+    protected String name;
+    protected int appetite;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BaseCat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
 }
