0ac9452937289cdaf2b7a1b2885fba780fd36a9b
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-08 16:44:35 +0300

    Modified class Plate

--- Plate.java
+++ Plate.java
@@ -1,12 +1,28 @@
 public class Plate {
 
     private int food;
-    
+
+    public int getFood() {
+        return food;
+    }
+    public void setFood(int food) {
+        this.food = food;
+    }
+   
     public Plate(int food) {
         this.food = food;
     }
     public void info() {
-        System.out.println("plate: " + food);
+        System.out.println("Осталось в тарелке: " + food);
     }
+
+    public void removeFood(int food) {
+        if (this.food - food >= 0) {
+            this.food -= food;
+        } 
+    }
+
+    public void addFood(int food) {
+        this.food += food;
 
 }
