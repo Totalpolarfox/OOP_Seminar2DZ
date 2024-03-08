d5e886dcbc5708a3346600a211c7814e359393c5
Totalpolarfox <totalpolarfox@outlook.com>
2024-03-08 16:46:41 +0300

    Added satiety method in class Cat

--- Cat.java
+++ Cat.java
@@ -1,6 +1,8 @@
 public class Cat {
     private String name;
     private int appetite;
+    private boolean satiety; // сытость
+    
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
